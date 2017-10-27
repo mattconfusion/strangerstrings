67148a0e3e84624ecb7eb0a1d1789966230d2943
Matteo Radice <mattconfusion@hotmail.com>
2017-10-27 21:47:41 +0200

    Added ts, js, min.js versions. Readme completed

--- README.md
+++ README.md
@@ -1,8 +1,22 @@
-                    ___ _____ ___    _   _  _  ___ ___ ___
-                   / __|_   _| _ \  /_\ | \| |/ __| __| _ \
-                   \__ \ | | |   / / _ \| .` | (_ | _||   /
-                   |___/_|_|_|_|_\/_/_\_\_|\_|\___|___|_|_\
-                       / __|_   _| _ \_ _| \| |/ __/ __|     
-                       \__ \ | | |   /| || .` | (_ \__ \     
-                       |___/ |_| |_|_\___|_|\_|\___|___/
-                          Strings from the UpsideDown     
+     ___ _____ ___    _   _  _  ___ ___ ___
+    / __|_   _| _ \  /_\ | \| |/ __| __| _ \
+    \__ \ | | |   / / _ \| .` | (_ | _||   /
+    |___/_|_|_|_|_\/_/_\_\_|\_|\___|___|_|_\
+        / __|_   _| _ \_ _| \| |/ __/ __|     
+        \__ \ | | |   /| || .` | (_ \__ \     
+        |___/ |_| |_|_\___|_|\_|\___|___/
+           Strings from the UpsideDown
+           uʍo◖ǝpısd∩ ǝɥʇ ɯoɹɟ sƃuıɹʇS     
+
+## Stranger Strings
+
+A pretty simple JS/TS library inspired by **Stranger Things**® and hacked together by looking at [**FileFormat**](http://www.fileformat.info/convert/text/upside-down-map.htm).
+
+### Usage
+
+```javascript
+  StrangerStrings.upsideDown('Strings from the UpsideDown');
+  // returns 'uʍo◖ǝpısd∩ ǝɥʇ ɯoɹɟ sƃuıɹʇS'
+```
+
+And beware the **real** [*Demogorgon*](https://en.wikipedia.org/wiki/Demogorgon_(Dungeons_%26_Dragons)!
---
+++ src/stranger.strings.js
@@ -0,0 +1,104 @@
+var StrangerStrings = /** @class */ (function () {
+    function StrangerStrings() {
+    }
+    /**
+    * Swap a JSON object keys and values
+    * @param {Object} json
+    * @return {Object}
+    */
+    StrangerStrings.swap = function (json) {
+        var ret = {};
+        for (var key in json) {
+            ret[json[key]] = key;
+        }
+        return ret;
+    };
+    /**
+     * Get dual direction flip table
+     * @return {Object}
+     */
+    StrangerStrings.getDoubleFlipTable = function () {
+        return Object.assign(StrangerStrings.flipTable, StrangerStrings.swap(StrangerStrings.flipTable));
+    };
+    /**
+    * Turn a string uspide down, and viceversa.
+    * @param {string} string
+    */
+    StrangerStrings.upsideDown = function (userString) {
+        var last = userString.length - 1;
+        var result = new Array(userString.length);
+        for (var i = last; i >= 0; --i) {
+            var c = userString.charAt(i);
+            var r = StrangerStrings.getDoubleFlipTable()[c];
+            result[last - i] = r != undefined ? r : c;
+        }
+        return result.join('');
+    };
+    /**
+     * Watch the light bulbs on the alphabet wall.
+     * @return {string} message from Will
+     */
+    StrangerStrings.watchLightBulbs = function () {
+        return 'R U N';
+    };
+    StrangerStrings.flipTable = {
+        '\u0021': '\u00A1',
+        '\u0022': '\u201E',
+        '\u0026': '\u214B',
+        '\u0027': '\u002C',
+        '\u0028': '\u0029',
+        '\u002E': '\u02D9',
+        '\u0033': '\u0190',
+        '\u0034': '\u152D',
+        '\u0036': '\u0039',
+        '\u0037': '\u2C62',
+        '\u003B': '\u061B',
+        '\u003C': '\u003E',
+        '\u003F': '\u00BF',
+        '\u0041': '\u2200',
+        '\u0042': '\u10412',
+        '\u0043': '\u2183',
+        '\u0044': '\u25D6',
+        '\u0045': '\u018E',
+        '\u0046': '\u2132',
+        '\u0047': '\u2141',
+        '\u004A': '\u017F',
+        '\u004B': '\u22CA',
+        '\u004C': '\u2142',
+        '\u004D': '\u0057',
+        '\u004E': '\u1D0E',
+        '\u0050': '\u0500',
+        '\u0051': '\u038C',
+        '\u0052': '\u1D1A',
+        '\u0054': '\u22A5',
+        '\u0055': '\u2229',
+        '\u0056': '\u1D27',
+        '\u0059': '\u2144',
+        '\u005B': '\u005D',
+        '\u005F': '\u203E',
+        '\u0061': '\u0250',
+        '\u0062': '\u0071',
+        '\u0063': '\u0254',
+        '\u0064': '\u0070',
+        '\u0065': '\u01DD',
+        '\u0066': '\u025F',
+        '\u0067': '\u0183',
+        '\u0068': '\u0265',
+        '\u0069': '\u0131',
+        '\u006A': '\u027E',
+        '\u006B': '\u029E',
+        '\u006C': '\u0283',
+        '\u006D': '\u026F',
+        '\u006E': '\u0075',
+        '\u0072': '\u0279',
+        '\u0074': '\u0287',
+        '\u0076': '\u028C',
+        '\u0077': '\u028D',
+        '\u0079': '\u028E',
+        '\u007B': '\u007D',
+        '\u203F': '\u2040',
+        '\u2045': '\u2046',
+        '\u2234': '\u2235'
+    };
+    return StrangerStrings;
+}());
